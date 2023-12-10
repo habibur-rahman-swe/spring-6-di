497adca75812ca52c3929aab1cbbad8e13dec9fd
habibur-rahman-swe <habibur0199mhr@gmail.com>
2023-12-10 20:00:11 +0600

    Initial Commete

---
+++ src/main/java/habib/springframework/springdi/Spring6DiApplication.java
@@ -0,0 +1,13 @@
+package habib.springframework.springdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6DiApplication.class, args);
+	}
+
+}
