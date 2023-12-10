8510b49d1fe3f459c87126edf82b81079ec8daa6
habibur-rahman-swe <habibur0199mhr@gmail.com>
2023-12-10 21:35:56 +0600

    Dependency injection using @Autowired

--- src/test/java/habib/springframework/springdi/Spring6DiApplicationTests.java
+++ src/test/java/habib/springframework/springdi/Spring6DiApplicationTests.java
@@ -1,11 +1,33 @@
 package habib.springframework.springdi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import habib.springframework.springdi.controllers.MyController;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+	
+	@Autowired
+	MyController myController;
+	
+	@Test
+	void testAutowirOfController() {
+		System.out.println(myController.sayHello());
+	}
+	
+	@Test
+	void testGetControllerFromCtx() {
+		MyController myController = applicationContext.getBean(MyController.class);
+		
+		System.out.println(myController.sayHello());
+	}
+	
 	@Test
 	void contextLoads() {
 	}
