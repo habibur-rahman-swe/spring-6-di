497b6d1657570543b0dbc7e66a2914964a51351f
habibur-rahman-swe <habibur0199mhr@gmail.com>
2023-12-10 21:19:52 +0600

    Spring context

--- src/main/java/habib/springframework/springdi/Spring6DiApplication.java
+++ src/main/java/habib/springframework/springdi/Spring6DiApplication.java
@@ -2,12 +2,22 @@ package habib.springframework.springdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import habib.springframework.springdi.controllers.MyController;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring6DiApplication.class, args);
+		
+		ApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+		
+		MyController controller = ctx.getBean(MyController.class);
+		
+		System.out.println("I'm Main Method");
+		
+		System.out.println(controller.sayHello());
 	}
 
 }
